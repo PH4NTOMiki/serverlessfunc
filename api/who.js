05c64d248f1f485d48a90d443e5fb903d112465e
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2020-01-30 13:06:10 +0100

    Update who.js

--- api/who.js
+++ api/who.js
@@ -1,13 +1,4 @@
 module.exports = (req, res) => {
-  let who = 'anonymous'
-
-  if (req.body && req.body.who) {
-    who = req.body.who
-  } else if (req.query.who) {
-    who = req.query.who
-  } else if (req.cookies.who) {
-    who = req.cookies.who
-  }
-
-  res.status(206).send(`Hello ${who}!`)
+	const who = (req.body && req.body.who)||req.query.who||req.cookies.who||'anonymous';
+	res.status(200).send(`Hello ${who}!`)
 }
